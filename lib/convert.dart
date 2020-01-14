cb152fd7b46400f51a57a0316529718e193a2351
chocovish <vishal.ghosh28@gmail.com>
2020-01-15 05:19:30 +0530

    initial

---
+++ lib/convert.dart
@@ -0,0 +1,16 @@
+// Copyright (c) 2018, the Zefyr project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// Provides codecs to convert Notus documents to other formats.
+library notus.convert;
+
+import 'src/convert/markdown.dart';
+import 'src/convert/html.dart';
+
+export 'src/convert/html.dart';
+export 'src/convert/markdown.dart';
+
+/// Markdown codec for Notus documents.
+const NotusMarkdownCodec notusMarkdown = const NotusMarkdownCodec();
+const NotusHTMLCodec notusHTML = const NotusHTMLCodec();
